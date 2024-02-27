9b0d70165c6dcb6d782cc66401022cd32b4800cc
Paul Scanlon <pauliescanlon@gmail.com>
2024-02-27 11:26:43 -0500

    refactor: inline path to endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.PROD ? 'https://tns-astro-site-search.netlify.app' : 'http://localhost:4321',
   integrations: [tailwind(), qwikdev(), mdx()],
 });
